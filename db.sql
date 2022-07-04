4cbec8b4848cf548e86d49a961fb058e07265fa3
shine102 <nguyenngotunglam2@gmail.com>
2022-07-04 17:06:11 +0700

    modify db.sql

--- db.sql
+++ db.sql
@@ -1,61 +1,68 @@
 -- create database
 create database SellWater;
-
 -- create table category_tab
-create table if not exists category_tab(
-    id int PRIMARY KEY AUTOINCREMENT,
+create table category_tab(
+    id int PRIMARY KEY IDENTITY(1,1),
     category_name varchar(128) not null,
     image_link varchar(256) not null
-)
+);
 -- create sample data
 
-
+INSERT INTO category_tab (category_name, image_link) 
+                  VALUES ('coffee', 'https://res.cloudinary.com/dimxnh34h/image/upload/v1656841765/category/coffee/coffee_pb08wk.png'); 
 
 
 -- create table drink_tab
-create table if not exists drink_tab(
-    id INT PRIMARY KEY AUTOINCREMENT,
+create table drink_tab(
+    id INT PRIMARY KEY IDENTITY(1,1),
     drink_name varchar(128) not null,
     price int not null,
     image_link varchar(256) not null,
     category_id int REFERENCES category_tab(id)
-    discount int not null
-)
+);
+
+-- create sample data
+
+INSERT INTO drink_tab (drink_name, price, image_link, category_id)  
+                VALUES('Ca phe sua da', 29000, 'https://res.cloudinary.com/dimxnh34h/image/upload/v1656927874/category/coffee/caphesuada_u9vbwb.jpg', 1);
 
 -- create table blog_tab
-create table if not exists blog_tab(
-    id int PRIMARY KEY AUTOINCREMENT,
+create table blog_tab(
+    id int PRIMARY KEY IDENTITY(1,1),
     blog_title varchar(1024) not null,
     content varchar(max) not null,
-    created_time datetime DEFAULT CURRENT_DATE(),
+    created_time datetime DEFAULT GetDate(),
     image_link varchar(256) not null,
-)
+);
+
+INSERT INTO blog_tab(blog_title, content, image_link)
+              VALUES('', '', '')
 
 -- create table order_tab
 
-create table if not exists order_tab(
-    id int primary key AUTOINCREMENT,
-    char_id varchar(50) not null,
+create table order_tab(
+    id int primary key IDENTITY(1,1),
+    char_id varchar(63),
     phone varchar(15) not null,
-    address varchar(256) not null,
-)
+    address varchar(256) not null
+);
 
 -- create table orderitem ( when 1 order have many item)
 
-create table if not exists order_item(
-    id int PRIMARY KEY AUTOINCREMENT,
+create table order_item(
+    id int PRIMARY KEY IDENTITY(1,1),
     order_id int REFERENCES order_tab(id),
-    drink_id int REFERENCES drink(id),
-    quantity int not null,
-)
+    drink_id int REFERENCES drink_tab(id),
+    quantity int not null
+);
 
 -- create table feedback
-create table if not EXISTS feedback_tab(
-    id int PRIMARY KEY AUTOINCREMENT,
+create table feedback_tab(
+    id int PRIMARY KEY IDENTITY(1,1),
     order_id int REFERENCES order_tab(id),
-    star int(1,5) default 5,
-    comment varchar(max) DEFAULT "",
-)
+    star int default 5,
+    comment varchar(max) DEFAULT '',
+);
 
 
 
