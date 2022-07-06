534ef758258d2c2131da1a026df629bc43a6ccf6
shine102 <nguyenngotunglam2@gmail.com>
2022-07-06 08:57:47 +0700

    create model, repo, controller, dto and many other thing

--- src/main/java/com/example/demo/sellwater/dto/CategoryDto.java
+++ src/main/java/com/example/demo/sellwater/dto/CategoryDto.java
@@ -1,5 +1,10 @@
 package com.example.demo.sellwater.dto;
 
+import lombok.Data;
+
+@Data
 public class CategoryDto {
-    
+    private Long id;
+    private String categoryName;
+    private String imageLink;
 }
