222130fdd42404a18bbb174d32aab1bca3758d64
shine102 <nguyenngotunglam2@gmail.com>
2022-07-09 02:22:17 +0700

    fix smthing

--- src/main/java/com/example/demo/sellwater/controller/MainController.java
+++ src/main/java/com/example/demo/sellwater/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.sellwater.model.BlogModel;
 import com.example.demo.sellwater.model.CategoryModel;
@@ -47,16 +48,39 @@ public class MainController {
         return "index";
     }
 
+    @GetMapping("/{categoryId}")
+    public String category(Model model, @PathVariable("categoryId") Long categoryId){
+        List<CategoryModel> categoryList = categoryService.fetchCategoryList();
+        List<BlogModel> blogList = blogService.fetchBlogList();
+        if (blogList.size() > 12) {
+            blogList = blogList.subList(0, 12);
+        }
+
+        List<DrinkModel> drinkList = drinkService.fetchDrinkListByCategoryId(categoryId);
+        if (drinkList.size() > 10) {
+            drinkList = drinkList.subList(0, 10);
+        }
+        model.addAttribute("drinkList", drinkList);
+        model.addAttribute("categoryList", categoryList);
+        model.addAttribute("blogList", blogList);
+        return "index";
+    }
+
     @GetMapping("/about")
     public String about(Model model){
         return "about";
     }
 
     @GetMapping("/drink")
-    public String drinks(Model model){
+    public String redirectDrink(){
+        return "redirect:/drink/1";
+    }
+
+    @GetMapping("/drink/{drinkId}")
+    public String drinks(Model model, @PathVariable("drinkId") Long drinkId){
         List<CategoryModel> categoryList = categoryService.fetchCategoryList();
         
-        List<DrinkModel> drinkList = drinkService.fetchDrinkListByCategoryId(categoryList.get(0).getId());
+        List<DrinkModel> drinkList = drinkService.fetchDrinkListByCategoryId(drinkId);
 
         model.addAttribute("drinkList", drinkList);
         model.addAttribute("categoryList", categoryList);
@@ -70,6 +94,13 @@ public class MainController {
         List<BlogModel> blogList = blogService.fetchBlogList();
         model.addAttribute("blogList", blogList);
         return "blogs";
+    }
+
+    @GetMapping("/blog/{blogId}")
+    public String article(Model model, @PathVariable Long blogId){
+        BlogModel article = blogService.fetchBlogById(blogId);
+        model.addAttribute("article", article);
+        return "article";
     }
 
 }
