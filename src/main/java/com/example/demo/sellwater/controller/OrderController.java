c4fbd9bd74df4fb619a25a739762e0e1aaa3e1be
shine102 <nguyenngotunglam2@gmail.com>
2022-07-12 14:06:16 +0700

    add order controller, admin controller

--- src/main/java/com/example/demo/sellwater/controller/OrderController.java
+++ src/main/java/com/example/demo/sellwater/controller/OrderController.java
@@ -1,5 +1,40 @@
 package com.example.demo.sellwater.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.sellwater.model.OrderModel;
+import com.example.demo.sellwater.service.OrderService;
+
+@Controller
 public class OrderController {
     
+    @Autowired
+    OrderService orderService = new OrderService();
+
+    @GetMapping("/order")
+    public String viewOrderSearchPage() {
+        return "order";
+    }
+
+    @PostMapping("/order")
+    public String searchOrder(String orderId, Model model){
+        OrderModel orderModel = orderService.searchOrder(orderId);
+        model.addAttribute("order", orderModel);
+        return "order";
+    }
+
+    @GetMapping("/order/create")
+    public String viewOrderPage() {
+        return "checkout";
+    }
+
+    @PostMapping("/order/create")
+    public String createOrder(OrderModel orderModel, Model model){
+        orderService.createOrder(orderModel);
+        return "checkout";
+    }
 }
--- src/main/java/com/example/demo/sellwater/service/OrderService.java
+++ src/main/java/com/example/demo/sellwater/service/OrderService.java
@@ -2,7 +2,16 @@ package com.example.demo.sellwater.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.sellwater.model.OrderModel;
+
 @Service
 public class OrderService {
+
+    public OrderModel searchOrder(String orderId) {
+        return null;
+    }
+
+    public void createOrder(OrderModel orderModel) {
+    }
     
 }
